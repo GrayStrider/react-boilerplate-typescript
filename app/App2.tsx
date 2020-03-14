1b236f23bc23673cd860dc9741475dd2fcd6ae3a
Gray Strider <zhoga.ivan@gmail.com>
2020-03-14 23:51:54 +1100

    split main app entry file for testing purposes

---
+++ app/App2.tsx
@@ -0,0 +1,20 @@
+import {FC, default as React} from 'react'
+import {translationMessages} from '@/translations/i18n'
+import {Provider} from 'react-redux'
+import LanguageProvider from '@/containers/LanguageProvider'
+import {ConnectedRouter} from 'connected-react-router'
+import history from '@/utils/history'
+import {store} from '@/store'
+
+export const App_: FC<{ messages?: any, Component?: FC }> =
+	({
+		 messages = translationMessages,
+		 Component = require ('./containers/App').default,
+	 }) =>
+		<Provider store={store} >
+			<LanguageProvider messages={messages} >
+				<ConnectedRouter history={history} >
+					<Component />
+				</ConnectedRouter >
+			</LanguageProvider >
+		</Provider >
--- app/app.tsx
+++ app/app.tsx
@@ -1,61 +1,26 @@
-/**
- * app.js
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
-
-// Import all the third party stuff
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {ConnectedRouter} from 'connected-react-router'
 import FontFaceObserver from 'fontfaceobserver'
-import history from '@/utils/history'
 import 'sanitize.css/sanitize.css'
-
-// Import root app
 import App from '@/containers/App'
-
-// Import Language Provider
-import LanguageProvider from '@/containers/LanguageProvider'
-
-// Load the favicon and the .htaccess file
-import '!file-loader?name=[name].[ext]!./static/images/favicon.ico'
-import 'file-loader?name=.htaccess!./.htaccess'
-
-import configureStore from '@/utils/redux-components/configureStore'
-
-// Import i18n messages
+// import '!file-loader?name=[name].[ext]!./static/images/favicon.ico'
+// import 'file-loader?name=.htaccess!./.htaccess'
 import {translationMessages} from '@/translations/i18n'
+import {App_} from '@/App2'
 
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
 const openSansObserver = new FontFaceObserver ('Open Sans', {})
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
 openSansObserver.load ().then (() => {
   document.body.classList.add ('fontLoaded')
 })
 
-// Create redux store with history
-const initialState = {}
-const store = configureStore (initialState, history)
 const MOUNT_NODE = document.getElementById ('app') as HTMLElement
+
 
 const render = (messages: any, Component = App) => {
   ReactDOM.render (
-    // tslint:disable-next-line:jsx-wrap-multiline
-    <Provider store={store} >
-      <LanguageProvider messages={messages} >
-        <ConnectedRouter history={history} >
-          <Component />
-        </ConnectedRouter >
-      </LanguageProvider >
-    </Provider >,
+    <App_ />,
     MOUNT_NODE,
   )
 }
