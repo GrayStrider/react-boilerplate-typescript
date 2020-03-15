0508820c6be576fc1216f1cd229f0ffb3a02d02d
Gray Strider <zhoga.ivan@gmail.com>
2020-03-15 14:07:51 +1100

    async findBy

--- app/tests/index.spec.tsx
+++ app/tests/index.spec.tsx
@@ -1,13 +1,12 @@
 import '@testing-library/jest-dom'
 import * as React from 'react'
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import AppWrapper from '@/wrapper'
 
 
 it ('should have features button', async () => {
   expect.assertions (1)
-  const app = render (<AppWrapper/>)
-  const act = app.queryByText ('Features')
+  const { findByText } = render (<AppWrapper />)
+  let act = await findByText ('Hello, world')
   expect (act).toBeInTheDocument ()
-
 })
